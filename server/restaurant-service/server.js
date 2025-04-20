d5516e80e8ad5dbf0660bc2f3e697cc81247ccdd
IT22099990 <it22099990@my.sliit.lk>
2025-04-20 12:12:44 +0530

    add cors

--- server/restaurant-service/server.js
+++ server/restaurant-service/server.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import restaurantRoutes from './routes/restaurant.routes.js';
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 // Database Connection
