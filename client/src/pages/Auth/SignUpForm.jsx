47e4bd16e72dccd63fc61dc8d025355b2a9af025
IT22099990 <it22099990@my.sliit.lk>
2025-04-20 19:59:35 +0530

    login and regsiter form

---
+++ client/src/pages/Auth/SignUpForm.jsx
@@ -0,0 +1,157 @@
+import React, { useState } from 'react'
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+const SignUpForm = () => {
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+        role: "",
+        contact: "",
+        address: ""
+    });
+
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const navigate = useNavigate();
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prev) => ({ ...prev, [name]: value }));
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setError(null);
+        setLoading(true);
+
+        if (formData.password !== formData.confirmPassword) {
+            setError("Passwords do not match.");
+            setLoading(false);
+            return;
+        }
+
+        try {
+            const { name, email, password, role, contact, address } = formData;
+            const userData =
+                role === "Customer" || role === "DeliveryPersonnel"
+                    ?
+                    { name, email, password, role, contact, address }
+                    :
+                    { name, email, password, role };
+
+            await axios.post("http://localhost:5000/auth/register", userData);
+            navigate("/login");
+
+        } catch (err) {
+            setError(err.response?.data?.message || "Sign up failed. Please try again.");
+            console.error("Sign up error:", error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+            <div className="max-w-md mt-10 ml-10 p-6 bg-white shadow-md rounded-md w-full md:w-1/2">
+                {error && (
+                    <div className="mb-4 p-3 bg-red-100 border border-red-400 text-red-700 rounded">
+                        {error}
+                    </div>
+                )}
+                {loading}
+                <form onSubmit={handleSubmit} className="space-y-4">
+                <input
+                        type="text"
+                        name="name"
+                        placeholder="Name"
+                        value={ formData.name}
+                        onChange={handleInputChange}
+                        className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        required
+                    />
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="Email"
+                        value={ formData.email}
+                        onChange={handleInputChange}
+                        className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        required
+                    />
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="Password"
+                        value={ formData.password}
+                        onChange={handleInputChange}
+                        className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        required
+                    />
+                    <input
+                        type="password"
+                        name="confirmPassword"
+                        placeholder="Confirm Password"
+                        value={ formData.confirmPassword}
+                        onChange={handleInputChange}
+                        className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        required
+                    />
+                    <select
+                        name="role"
+                        value = {formData.role}
+                        onChange={handleInputChange}
+                        className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        required
+                    >
+                        <option value="" disabled>Select your role</option>
+                        <option value="Customer">Customer</option>
+                        <option value="RestaurantAdmin">Restaurant Admin</option>
+                        <option value="DeliveryPersonnel">Delivery Personnel</option>
+                    </select>
+
+                    {(formData.role === "Customer" || formData.role === "DeliveryPersonnel") && (
+                        <div className = "space-y-4">
+                            <input
+                                type="text"
+                                name="contact"
+                                placeholder="Contact Number"
+                                value={ formData.contact}
+                                onChange={handleInputChange}
+                                className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                required
+                            />
+                            <input
+                                type="text"
+                                name="address"
+                                placeholder="Address"
+                                value={ formData.address}
+                                onChange={handleInputChange}
+                                className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                            />
+                        </div>
+                    )}
+
+                    <div className="items-center justify-center flex">
+                        <p className="text-sm text-gray-600">
+                            Already have an Account?{' '}
+                            <a href="/login" className="text-blue-600 hover:underline hover:text-blue-800 transition">
+                                Login
+                            </a>
+                        </p>
+                    </div>
+                    <button
+                        type="submit"
+                        className="mt-4 w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 disabled:bg-blue-300"
+                    >
+                        Sign Up
+                    </button>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default SignUpForm
