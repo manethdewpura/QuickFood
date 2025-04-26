1dccf8345b344b2bb871144bc733feab9d77fab2
IT22099990 <it22099990@my.sliit.lk>
2025-04-26 12:42:33 +0530

    feat: Add restaurant verification update functionality and corresponding route

--- server/restaurant-service/routes/restaurantAdmin.route.js
+++ server/restaurant-service/routes/restaurantAdmin.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { getAllRestaurants } from '../services/restaurant.service.js';
+import { getAllRestaurants, updateVerification } from '../services/restaurant.service.js';
 
 const router = express.Router();
 
 router.get('/all', getAllRestaurants); // Get all restaurants
+router.put('/verify/:id', updateVerification); // Update restaurant verification status by ID
 
 export default router;
